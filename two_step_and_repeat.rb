db98056855ed5c3c059e2eb6eeec48f918dbaa81
barnesemily12@gmail.com <barnesemily12@gmail.com>
2018-07-24 20:44:28 +0000

    automatically backed up by learn

--- two_step_and_repeat.rb
+++ two_step_and_repeat.rb
@@ -59,9 +59,9 @@ a_few_more_steps
 
 # def how_many_steps?
 #   # Write a loop that fulfills all criteria from Part 3 of the README.md
-
+def how_many_steps
 steps = 0 
-
+puts 
 
 
 # def break_dance
