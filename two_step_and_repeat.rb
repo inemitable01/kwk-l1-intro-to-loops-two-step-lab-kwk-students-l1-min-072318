1efcfaba80e74b86e027afb7084df7280804e452
barnesemily12@gmail.com <barnesemily12@gmail.com>
2018-07-24 20:48:45 +0000

    automatically backed up by learn

--- two_step_and_repeat.rb
+++ two_step_and_repeat.rb
@@ -60,10 +60,10 @@ a_few_more_steps
 # def how_many_steps?
 #   # Write a loop that fulfills all criteria from Part 3 of the README.md
 def how_many_steps
-steps = 0 
-steps += 1
+steps == 0 
+steps +== 1
 steps= 8
-puts 8 % 2 = 4
+puts 8 % 2 == 4
 sleep (0.5)
 end
 end
